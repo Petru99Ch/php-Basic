9ea1d392b93f6f2791be5ec63472ef903ef38cfe
Petru99Ch <117940325+Petru99Ch@users.noreply.github.com>
2024-02-18 17:40:11 +0200

    18.02

--- src/index.php
+++ src/index.php
@@ -1,25 +1,26 @@
-<h1>Welcome to our HOTEL booking</h1>
-<?php 
-    $days_till_openning = 30 ; 
-    $bonusPlaces = 10; 
+<!-- Data Base -->
 
-    // HW: 
-    $city  = "Chisinau";
-    $street = "Str. Mihail Sadoveanu 4/1";
-    $postal_code ="MD-2044";  
-    $phone = "060066691";
-    $email = "booking@example.com";
-    $geo_cord = "47.036930, 28.878878";
+<? 
+// HW: Constants
+    define("PAGE_TITLE" , "Booking confirmation");
+    define("SEAT_PRICE" , "100.50");
+   
 
+    $book_client_vip = true;
+    $book_adults = 2; 
 
+    // HW: format the total cost: 111.11 
+    $book_cost =  number_format( $book_adults * SEAT_PRICE, 2, ".", " ");
     
+
 ?>
 
-<p>To reserve, contact us</p>
-<p>City: <?php print($city) ?></p>
-<p>Adress: <?php print($street." ".$postal_code); ?></p>
-<p>Phone: <a href="tel:+37360066691"><?php print($phone) ?></a></p>
-<p>Send message to email: <a href="mailto:booking@example.com"><?php print($email) ?></a></p>
-<p>Open adress to map: <a href="https://maps.app.goo.gl/LjGwitmaVtZD74jS6"> <?php print($geo_cord) ?></a></p>
 
+<!-- Template/ View -->
+<h1><?= PAGE_TITLE ?></h1>
+<p>Adults: <?= $book_adults ?></p>
+<p>Total cost: <?= $book_adults?> x <?= SEAT_PRICE?> = <?= $book_cost ?></p>
 
+<? if($book_client_vip): ?>
+    <p>VIP client</p>
+<? endif ?>
